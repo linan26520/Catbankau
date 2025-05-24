74c264a946d0e25a65c0343a55468edf9bdb1c75
linan26520 <linan26520@gmail.com>
2025-05-25 04:55:36 +1000

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCHsJpZzzev5pBGirCfhXZN0Bw1zKcCsk",
+  authDomain: "catbankau.firebaseapp.com",
+  projectId: "catbankau",
+  storageBucket: "catbankau.appspot.com",
+  messagingSenderId: "694383874365",
+  appId: "1:694383874365:web:daea715fef30e8fbeaf554"
+};
+firebase.initializeApp(firebaseConfig);
